b588be5f64a454317039a6401e931020abb52232
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2025-03-22 10:25:38 +0800

    test

--- Arrowgene.DJMaxOnline.Server/DjMaxCrypto.cs
+++ Arrowgene.DJMaxOnline.Server/DjMaxCrypto.cs
@@ -24,7 +24,8 @@ public class DjMaxCrypto
         public void Reset()
         {
             _mt = new MersenneTwister(_mtSeed);
-            UpdateSumBuffer(_sumSeed, 0);
+            BinaryPrimitives.WriteUInt32LittleEndian(_sumBuffer, _sumSeed);
+            BinaryPrimitives.WriteUInt32LittleEndian(_sumBuffer[4..], 0);
         }
 
         public ReadOnlySpan<byte> NextRngBuffer()
@@ -35,16 +36,9 @@ public class DjMaxCrypto
             return new ReadOnlySpan<byte>(_rngBuffer);
         }
 
-        public void UpdateSumBuffer(uint first, uint second)
+        public Span<byte> GetSumBuffer()
         {
-            Span<byte> span = _sumBuffer;
-            BinaryPrimitives.WriteUInt32LittleEndian(span, first);
-            BinaryPrimitives.WriteUInt32LittleEndian(span[4..], second);
-        }
-
-        public ReadOnlySpan<byte> GetSumBuffer()
-        {
-            return new ReadOnlySpan<byte>(_sumBuffer);
+            return new Span<byte>(_sumBuffer);
         }
     }
 
@@ -59,74 +53,97 @@ public class DjMaxCrypto
 
     public void Decrypt(ref Span<byte> data)
     {
-        Crypt(ref data, _dec);
-    }
-
-    public void Encrypt(ref Span<byte> data)
-    {
-        Crypt(ref data, _enc);
-    }
-
-    private void Crypt(ref Span<byte> data, DjMaxCryptoState state)
-    {
-        ReadOnlySpan<byte> rng = state.NextRngBuffer();
-        ReadOnlySpan<byte> sum = state.GetSumBuffer();
+        ReadOnlySpan<byte> rng = _dec.NextRngBuffer();
+        Span<byte> sum = _dec.GetSumBuffer();
         int idx = 0;
-        
-        uint ebp_10 = BinaryPrimitives.ReadUInt32LittleEndian(data[4..]);
-        uint ebp_4 = BinaryPrimitives.ReadUInt32LittleEndian(data[8..]);
-        
+
         for (int i = 0; i < data.Length; i++)
         {
             if (idx > 7)
             {
-                uint edi = 0;
-                uint eax = 0; //  delta sum
-                uint esi = 0;
-                uint edx = BinaryPrimitives.ReadUInt32LittleEndian(sum);
-                uint ecx = BinaryPrimitives.ReadUInt32LittleEndian(sum[4..]);
-
-
-                uint ebp_8 = ebp_10;
-                uint ebp_c = ebp_4;
-
-                for (int j = 0; j < 32; j++)
-                {
-                    eax = edx;
-                    eax = eax >> 5;
-                    eax = eax + ebp_10; // ebp+10 //07 00 00 00
-                    edi = edx;
-                    edi = edi << 4;
-                    edi = edi + ebp_4; // ebp-4 // AB 64 AB C8
-                    esi = esi - 0x61C88647;
-                    eax = eax ^ edi;
-                    edi = esi + edx; // [esi + edx]
-                    eax = eax ^ edi;
-                    ecx = ecx + eax;
-
-                    eax = ecx;
-                    eax = eax >> 5;
-                    eax = eax + ebp_8; // ebp-8
-                    edi = ecx;
-                    edi = edi << 4;
-                    edi = edi + ebp_c; // ebp-C
-                    eax = eax ^ edi;
-                    edi = esi + ecx;
-                    eax = eax ^ edi;
-                    edx = edx + eax;
-                }
-                
+                uint clearBlock2 = BinaryPrimitives.ReadUInt32LittleEndian(data[(i - 4)..]);
+                uint clearBlock1 = BinaryPrimitives.ReadUInt32LittleEndian(data[(i - 8)..]);
+                Crypt(ref sum, clearBlock2, clearBlock1);
                 idx = 0;
-                state.UpdateSumBuffer(ecx, edx);
-                rng = state.NextRngBuffer();
-                
-                ebp_10 = BinaryPrimitives.ReadUInt32LittleEndian(data[(i-4)..]);
-                ebp_4 = BinaryPrimitives.ReadUInt32LittleEndian(data[(i-8)..]);
+                rng = _dec.NextRngBuffer();
             }
 
             byte key = (byte)(sum[idx] ^ rng[idx]);
             data[i] = (byte)(data[i] ^ key);
             idx++;
         }
+    }
+
+    public void Encrypt(ref Span<byte> data)
+    {
+        ReadOnlySpan<byte> rng = _enc.NextRngBuffer();
+        Span<byte> sum = _enc.GetSumBuffer();
+        int idx = 0;
+
+        uint clearBlock2 = BinaryPrimitives.ReadUInt32LittleEndian(data[4..]);
+        uint clearBlock1 = BinaryPrimitives.ReadUInt32LittleEndian(data);
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (idx > 7)
+            {
+                Crypt(ref sum, clearBlock2, clearBlock1);
+                idx = 0;
+                rng = _enc.NextRngBuffer();
+                if (i + 4 + 4 < data.Length)
+                {
+                    clearBlock2 = BinaryPrimitives.ReadUInt32LittleEndian(data[(i + 4)..]);
+                    clearBlock1 = BinaryPrimitives.ReadUInt32LittleEndian(data[i..]);
+                }
+            }
+
+            byte key = (byte)(sum[idx] ^ rng[idx]);
+            data[i] = (byte)(data[i] ^ key);
+            idx++;
+        }
+    }
+
+    private void Crypt(ref Span<byte> sum, uint clearBlock2, uint clearBlock1)
+    {
+        uint edi = 0;
+        uint eax = 0;
+        uint esi = 0;
+        uint edx = BinaryPrimitives.ReadUInt32LittleEndian(sum[4..]);
+        uint ecx = BinaryPrimitives.ReadUInt32LittleEndian(sum);
+
+        // Console.WriteLine($"clearBlock2:{clearBlock2}");
+        // Console.WriteLine($"clearBlock1:{clearBlock1}");
+        // Console.WriteLine($"edx:{edx}");
+        // Console.WriteLine($"ecx:{ecx}");
+        // Console.WriteLine($"---------");
+
+        for (int j = 0; j < 32; j++)
+        {
+            eax = edx;
+            eax = eax >> 5;
+            eax = eax + clearBlock2;
+            edi = edx;
+            edi = edi << 4;
+            edi = edi + clearBlock1;
+            esi = esi - 0x61C88647;
+            eax = eax ^ edi;
+            edi = esi + edx;
+            eax = eax ^ edi;
+            ecx = ecx + eax;
+
+            eax = ecx;
+            eax = eax >> 5;
+            eax = eax + clearBlock2;
+            edi = ecx;
+            edi = edi << 4;
+            edi = edi + clearBlock1;
+            eax = eax ^ edi;
+            edi = esi + ecx;
+            eax = eax ^ edi;
+            edx = edx + eax;
+        }
+
+        BinaryPrimitives.WriteUInt32LittleEndian(sum, ecx);
+        BinaryPrimitives.WriteUInt32LittleEndian(sum[4..], edx);
     }
 }
